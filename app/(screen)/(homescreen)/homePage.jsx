e5c97bad6562d4daa6b96ef79511f5c440557566
Charan <charanbodagala05@gmail.com>
2024-09-05 06:32:40 +0530

    commit

---
+++ app/(screen)/(homescreen)/homePage.jsx
@@ -0,0 +1,59 @@
+import {React, useState, useEffect} from 'react';
+import MapView , {Marker} from 'react-native-maps';
+import { StyleSheet, View ,Text , Dimensions,Button} from 'react-native';
+import * as Location from 'expo-location'
+import { StatusBar } from 'expo-status-bar';
+
+export default function HomePage() {
+
+  const [mapRegion ,setMapRegion] = useState({
+    latitude : 13.08784000,
+    longitude : 80.27847000,
+    longitudeDelta : 0.0421,
+    latitudeDelta : 0.0922
+  })
+
+  const userLocation = async () => {
+    let status = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted'){
+      console.log('Permission to access location was denied');
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setMapRegion({
+      latitude : location.coords.latitude,
+      longitude : location.coords.longitude,
+      longitudeDelta : 0.0421,
+      latitudeDelta : 0.0922
+    });
+    console.log(location.coords.latitude,location.coords.longitude);
+  }
+
+
+  useEffect(() => {
+    userLocation();
+  },[]);
+
+  return (
+    <View style={styles.container}>
+
+      <MapView style={styles.map} 
+      region={mapRegion}
+      >
+        <Marker coordinate={mapRegion} title='Marker'/>
+      </MapView>
+      <Button title='Get Location' onPress={userLocation}/>
+      <StatusBar style='auto'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '95%',
+  },
+});
